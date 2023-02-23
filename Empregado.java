2c4ed4ddbd601413302468b824ee525b90e37f6d
Danilo Lopes <daniloinfinitbr@gmail.com>
2023-02-23 17:25:58 -0300

    Calculo de salario efetuado de acordo com o tipo

--- Empregado.java
+++ Empregado.java
@@ -1,7 +1,22 @@
 public class Empregado{
+    private int tipo;
     private String nome;
     private int idade;
-    
+    private double salario;
+    private double comissao;
+    private double bonus;
+
+    public double calcularSalario(){
+        if (tipo == 0) 
+            return salario;
+        if (tipo == 1)
+            return salario + salario * comissao;
+        if (tipo == 2)
+            return salario + bonus;
+        return 0;
+
+    }
+
     public String getNome(){
         return nome;
     }
